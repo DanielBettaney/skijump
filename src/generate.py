57745e4051a7f37dfec8f2f382b921154c3a44f1
SaikumarChinthakayala <saikumar.sssihl@gmail.com>
2025-03-22 13:00:30 +0200

    implemented read json method in mt branch

--- src/generate.py
+++ src/generate.py
@@ -65,11 +65,15 @@ class SkiJump:
     # object.
     def from_json_file(path: pathlib.Path):
         """Read configuration from JSON file."""
+        f__= open(path, "r")
+        params_= json.load(f__)
+        params_= dict(params_)
+        skijumpobj= SkiJump(**params_)
         # Work here in Step 1!
         # Create a `SkiJump` object with the specification given in the file.
         # The `dataclass` decorator adds, e.g., a constructor with keyword arguments,
         # as is used above for creating the `Hill` object.
-        raise NotImplementedError()
+        raise skijumpobj
 
     def landing(self, hill: Hill) -> float:
         """Returns the intersection of the trajectory and the hill."""
